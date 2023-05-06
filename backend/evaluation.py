7fb33e6ed560d019a8c540fc31fbaba161e14634
RyunosukeIchiyasu <ryunosukeichiyasu@gmail.com>
2023-05-06 09:10:44 +0900

    modified SPA app

--- backend/evaluation.py
+++ backend/evaluation.py
@@ -1,39 +1,9 @@
-import csv
-import xml.etree.ElementTree as ET 
 import numpy as np
 
 class Evaluator:
     def __init__(self):
         self.inferlist = []
         self.gtlist = []
-
-    def readInfer(self, PATH_TO_INFER_RESULT):
-        with open(PATH_TO_INFER_RESULT) as f:
-            reader = csv.DictReader(f)
-            self.inferlist = [row for row in reader]
-        for infer in self.inferlist:
-            infer['filename']=infer['filename'].split('.')[0]
-
-    def readGT(self, PATH_TO_ANNOTATION_DIR, PATH_TO_TEST_LIST):
-        with open(PATH_TO_TEST_LIST, "r") as f:
-            lines = f.read().splitlines()
-        GT_PATHS = [PATH_TO_ANNOTATION_DIR + line.split('.')[0] + '.xml' for line in lines]
-        
-        for index, gt_path in enumerate(GT_PATHS):
-            root = ET.parse(gt_path).getroot()
-            for child in root:
-                if child.tag == 'object':
-                    gt = {}
-                    gt['filename'] = gt_path.split('/')[-1].split('.')[0]
-                    for tag in child:
-                        if tag.tag=='name': gt['class'] = tag.text
-                        if tag.tag=='bndbox':
-                            for bbox in tag:
-                                if bbox.tag=='xmin': gt['left'] = bbox.text
-                                if bbox.tag=='xmax': gt['right'] = bbox.text
-                                if bbox.tag=='ymin': gt['top'] = bbox.text
-                                if bbox.tag=='ymax': gt['bottom'] = bbox.text
-                    self.gtlist.append(gt)
 
     def iou(self, a, b):
         # input shape : a, b =[xmin, ymin, xmax, ymax]
@@ -157,27 +127,4 @@ class Evaluator:
         return results, best_score
 
 if __name__ == '__main__':
-    IOU_TH = 0.5
-    PATH_TO_INFER_RESULT = "C:/work/infer/out/infer_results.csv"
-    PATH_TO_ANNOTATION_DIR = "C:/work/infer/data/"
-    PATH_TO_TEST_LIST = "C:/work/infer/data/test.txt"
-    CLASS_LIST = ['person', 'car']
-    
-    evaluator = Evaluator()
-    evaluator.readInfer(PATH_TO_INFER_RESULT)
-    evaluator.readGT(PATH_TO_ANNOTATION_DIR, PATH_TO_TEST_LIST)
-    results = []
-    for SCORE_TH in np.arange(0.00, 1.00, 0.01):
-        result = {}
-        pairlist_infer = evaluator.findPairInferToGT(SCORE_TH, IOU_TH, CLASS_LIST)
-        pairlist_GT = evaluator.findPairGTToInfer(SCORE_TH, IOU_TH, CLASS_LIST)
-        result['score'] = SCORE_TH
-        result['infers'] = len(pairlist_infer)
-        result['gts'] = len(pairlist_GT)
-        result['TP_infer'], result['TP_gt'] = evaluator.ConfusionMatrix(pairlist_infer, pairlist_GT)
-        result['precision'] = round(result['TP_infer']/result['infers'], 3) if result['infers']!=0 else 0.00
-        result['recall'] = round(result['TP_gt']/result['gts'], 3) if result['gts']!=0 else 0.00
-        result['f1'] = round(2*result['precision']*result['recall']/(result['precision']+result['recall']), 3) if result['infers']!=0 and result['gts']!=0 else 0.00
-        results.append(result)
-        print(result)
     pass
---
+++ frontend/src/components/EvalSetting.vue
@@ -0,0 +1,124 @@
+<template>
+  <div>
+    <div>
+      <h1 class="fs-4">GT List</h1>
+      <!-- <div style="text-align: right;"><v-icon icon="mdi mdi-plus-box" @click="dialog_addGT = true"></v-icon></div> -->
+      <table class="table table-dark table-striped">
+        <thead>
+          <tr>
+            <th></th>
+            <th>ID</th>
+            <th>GT Name</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="gt in gt_list" :key="gt.gt_id">
+            <td style="text-align: center;"><input v-model="selected_gt_id" class="form-check-input" type="radio" :value="gt.gt_id" :checked="gt_id === gt.gt_id"></td>
+            <td>{{ gt.gt_id }}</td>
+            <td>{{ gt.gt_name }}</td>
+            <td style="text-align: center;"><v-icon icon="mdi mdi-trash-can" @click="deleteList('gt_'+gt.gt_id)"></v-icon></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div>
+      <h1 class="fs-4">Infer List</h1>
+      <table class="table table-dark table-striped">
+        <thead>
+          <tr>
+            <th></th>
+            <th>ID</th>
+            <th>Model Name</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="infer in infer_list" :key="infer.infer_id">
+            <td style="text-align: center;"><input v-model="selected_infer_id" class="form-check-input" type="radio" :value="infer.infer_id" :checked="infer_id === infer.infer_id"></td>
+            <td>{{ infer.infer_id }}</td>
+            <td>{{ infer.model_name }}</td>
+            <td style="text-align: center;"><v-icon icon="mdi mdi-trash-can" @click="deleteList('infer_'+infer.infer_id)"></v-icon></td>
+          </tr>
+        </tbody>
+      </table>
+      <input type="hidden" name="class_iou_list" value='[{"class_name":"all", "iou_th":0.5}]'>
+      <v-btn variant="flat" color="blue" @click="getEvaluateResult()">Evaluate</v-btn>
+    </div>
+  
+    <div>
+      <table class="table table-dark table-striped">
+        <thead>
+          <tr>
+            <th>Class</th>
+            <th>IoU Threshold</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="class_iou in class_iou_list" :key="class_iou.class_name" style="vertical-align: middle;">
+            <td><v-switch small v-model="class_iou.checked" color="info" :label="class_iou.class_name" :value="true" hide-details :model-value="class_iou.checked"></v-switch></td>
+            <td><input class="form-range" type="range" v-model="class_iou.iou_th" max="0.9" min="0.1" step="0.1" hide-details></td>
+            <td>{{class_iou.iou_th}}</td>
+          </tr>
+        </tbody>
+      </table>
+      <v-btn variant="flat" color="blue" @click="updateClassIouList()">Apply</v-btn>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'EvalSetting',
+
+  props: ['gt_id', 'infer_id', 'class_iou_list'],
+  
+  data() {
+    return {
+      selected_gt_id: null,
+      selected_infer_id: null,
+      gt_list: [],
+      infer_list: [],
+    };
+  },
+
+  methods: {
+    getList(){
+      axios.get('http://localhost:5000/getList')
+      .then(response => {
+        this.gt_list = response.data.gt_list;
+        this.infer_list = response.data.infer_list;
+        console.log('succeeded /getList');
+      })
+    },
+    deleteList(value){
+      axios.get('http://localhost:5000/deleteList', {
+        params: {
+          delete_info: value,
+        }
+      })
+      .then(() => {
+        console.log('succeeded /deleteList');
+        this.getList()
+      })
+    },
+    getEvaluateResult(){
+      const new_gt_id = this.selected_gt_id;
+      const new_infer_id = this.selected_infer_id;
+      this.$emit('clicked-evaluate', new_gt_id, new_infer_id)
+    },
+    updateClassIouList(){
+      this.$emit('clicked-apply', false);
+    },
+  },
+
+  mounted(){
+    this.getList();
+  },
+}
+</script>
